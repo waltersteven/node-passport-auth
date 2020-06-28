36d9a6c48ba661cbb7c1d9a309a41131e994e4e6
Walter Pariona <wltrst97@gmail.com>
2020-06-28 15:02:47 -0500

    Added logic to store Users in Atlas

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/User');
 
 // Login page
-router.get('/login', (req, res) => res.send('Login'));
+router.get('/login', (req, res) => res.render('login'));
 
 // Register page
-router.get('/register', (req, res) => res.send('Register'));
+router.get('/register', (req, res) => res.render('register'));
+
+router.post('/register', (req, res) => {
+	const { name, email, password, confirm_password } = req.body;
+
+	let errors = [];
+
+	// Check required fields
+	if (!name || !email || !password || !confirm_password) {
+		errors.push({ msg: 'Please fill in all fields' });
+	}
+
+	// Check passwords match
+	if (password !== confirm_password) {
+		errors.push({ msg: 'Passwords do not match' });
+	}
+
+	// Check password length
+	if (password.length < 6) {
+		errors.push({ msg: 'Password should be at least 6 characters' });
+	}
+
+	if (errors.length > 0) {
+		res.render('register', {
+			errors,
+			name,
+			email,
+			password,
+			confirm_password
+		});
+	} else {
+		const newUser = new User({
+			name,
+			email,
+			password
+		});
+
+		// Hash password
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(newUser.password, salt, (err, hash) => {
+				if (err) throw err;
+
+				// Set password to hashed
+				newUser.password = hash;
+
+				// Save user
+				newUser.save()
+				.then( user => res.redirect('/users/login'))
+				.catch(err => console.log('err :>> ', err));
+			})
+		})
+	}
+});
 
 
 module.exports = router;
