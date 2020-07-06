53a5399ba27d6d6595fa9c7f025c069838509333
Walter Pariona <wltrst97@gmail.com>
2020-07-05 23:09:50 -0500

    Added custom middleware to ensure authentication per route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 // Welcome page
 router.get('/', (req, res) => res.render('welcome'));
 
-// Dashboard page
-router.get('/dashboard', (req, res) => res.render('dashboard'));
+// Dashboard page (contains ensureAuthenticated middleware).
+router.get('/dashboard', ensureAuthenticated, (req, res) => 
+
+res.render('dashboard', {
+	name: req.user.name
+}));
 
 module.exports = router;
